2a0deda3217eb36d98cd0d02264729f42afa7e41
Roy Ash <roy.ash456@gmail.com>
2024-07-22 18:47:55 +0300

    reading the file via `FileChannel` instead of `SeekableByteChannel` & allocating new very large `ByteBuffer`
    Signed-off-by: Roy Ash <roy.ash456@gmail.com>

--- src/main/java/io/github/ashr123/hash/verifier/HashVerifier.java
+++ src/main/java/io/github/ashr123/hash/verifier/HashVerifier.java
@@ -3,14 +3,16 @@ package io.github.ashr123.hash.verifier;
 import picocli.CommandLine;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.channels.SeekableByteChannel;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.security.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HexFormat;
@@ -22,34 +24,26 @@ import java.util.stream.Stream;
 		versionProvider = HashVerifier.class,
 		showDefaultValues = true,
 		description = "Verify or generate file's hash with given hash.")
-public class HashVerifier implements CommandLine.IVersionProvider
-{
-	private HashVerifier()
-	{
+public class HashVerifier implements CommandLine.IVersionProvider {
+	private HashVerifier() {
 	}
 
-	public static void main(String... args)
-	{
+	public static void main(String... args) {
 		new CommandLine(HashVerifier.class).execute(args);
 	}
 
-	public static byte[] digest(Path path, MessageDigest messageDigest) throws IOException
-	{
-		try (SeekableByteChannel seekableByteChannel = Files.newByteChannel(path, EnumSet.of(StandardOpenOption.READ)))
-		{
-			final ByteBuffer buffer = ByteBuffer.allocateDirect(Math.toIntExact(Math.min(
-							Integer.MAX_VALUE,
-							Math.min(
-									Runtime.getRuntime().maxMemory(),
-									seekableByteChannel.size()
-							)
-					)))
-					.order(ByteOrder.nativeOrder());
-			while (seekableByteChannel.read(buffer) != -1)
-			{
-				buffer.flip();
-				messageDigest.update(buffer);
-				buffer.clear();
+	public static byte[] digest(Path path, MessageDigest messageDigest) throws IOException {
+		try (FileChannel fileChannel = FileChannel.open(path, EnumSet.of(StandardOpenOption.READ))) {
+			final long size = fileChannel.size();
+			long position = 0;
+
+			while (position < size) {
+				final long bytesToMap = Math.min(Integer.MAX_VALUE, size - position);
+
+				messageDigest.update(fileChannel.map(FileChannel.MapMode.READ_ONLY, position, bytesToMap)
+						.order(ByteOrder.nativeOrder()));
+
+				position += bytesToMap;
 			}
 		}
 		return messageDigest.digest();
@@ -70,14 +64,11 @@ public class HashVerifier implements CommandLine.IVersionProvider
 					converter = PathConverter.class,
 					description = "Path to file for digestion.")
 			Path pathToFile
-	)
-	{
+	) {
 		System.err.println("Generating hash...");
-		try
-		{
+		try {
 			System.out.println(HexFormat.of().formatHex(digest(pathToFile, algorithm)));
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			//noinspection ThrowablePrintedToSystemOut
 			System.err.println(e);
 		}
@@ -101,42 +92,34 @@ public class HashVerifier implements CommandLine.IVersionProvider
 			@CommandLine.Parameters(paramLabel = "hash",
 					description = "Hash string to verify against.")
 			String hash
-	)
-	{
+	) {
 		System.err.println("Starting hash verification...");
-		try
-		{
+		try {
 			System.out.println(MessageDigest.isEqual(
 					digest(pathToFile, algorithm),
 					HexFormat.of().parseHex(hash)
 			));
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			//noinspection ThrowablePrintedToSystemOut
 			System.err.println(e);
 		}
 	}
 
 	@Override
-	public String[] getVersion()
-	{
+	public String[] getVersion() {
 		return new String[]{"Hash Verifier v" + getClass().getPackage().getImplementationVersion()};
 	}
 
-	private static class MessageDigestConverter implements CommandLine.ITypeConverter<MessageDigest>
-	{
+	private static class MessageDigestConverter implements CommandLine.ITypeConverter<MessageDigest> {
 		@Override
-		public MessageDigest convert(String value) throws NoSuchAlgorithmException
-		{
+		public MessageDigest convert(String value) throws NoSuchAlgorithmException {
 			return MessageDigest.getInstance(value);
 		}
 	}
 
-	private static class PathConverter implements CommandLine.ITypeConverter<Path>
-	{
+	private static class PathConverter implements CommandLine.ITypeConverter<Path> {
 		@Override
-		public Path convert(String value) throws IOException
-		{
+		public Path convert(String value) throws IOException {
 			final Path path = Path.of(value);
 			if (Files.notExists(path))
 				throw new NoSuchFileException(value);
@@ -146,11 +129,9 @@ public class HashVerifier implements CommandLine.IVersionProvider
 		}
 	}
 
-	private static class AlgorithmsName implements Iterable<String>
-	{
+	private static class AlgorithmsName implements Iterable<String> {
 		@Override
-		public Iterator<String> iterator()
-		{
+		public Iterator<String> iterator() {
 			final String simpleName = MessageDigest.class.getSimpleName();
 			return Stream.of(Security.getProviders()).parallel().unordered()
 					.map(Provider::getServices)
